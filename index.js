0e3d2126c2f81b96d34951ef7f39b84b026f047d
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-23 00:33:02 +0300

    commit tree 0e84230d7b22a898b3f7f6422c23d9cd53a97b3d

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+for(let i=0;i< word.length/2;i++){
+const j = word.length-1-i;
+const startChar = word[i];
+const endChar = word[j];
+if(startChar !== endChar) return false
+return true;
+}
 }
 
 /* 
   Add your pseudocode here
+    We should check if the first letter of a given word is the same as the last 
+    letter and if the second last is the same at the secondly positioned letter.
+    We should carry on until we have compared come across a letter that doesn't match its corresponding- 
+    equally positioned letter when the string is read from the behind.
+    At that point we halt the comparison and return false.
+    If all the letters match after all comparison, we conclude that the word is a palindrome and return true.
+
 */
 
 /*
   Add written explanation of your solution here
+  refactoring the code for redundancy
+  for(let i=0;i<word.length/2;i++){
+   const j = word.length-1-i;
+   const startChar = word[i];
+   const endChar = word[j];
+   return startChar !== endChar
+}
 */
 
 // You can run `node index.js` to view these console logs
@@ -18,8 +39,30 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+
 }
 
 module.exports = isPalindrome;
